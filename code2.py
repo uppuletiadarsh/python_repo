f95ad2d12061aec6dae10c92bc81d1649fb87022
uppuleti adarsh <adarshuppuleti@gmail.com>
2024-07-10 16:53:49 +0530

    'first'

---
+++ code2.py
@@ -0,0 +1,16 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num == 2:
+        return True  
+    else:
+        
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+number = 17
+if is_prime(number):
+    print(f'{number} is a prime number')
+else:
+    print(f'{number} is not a prime number')
